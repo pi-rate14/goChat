4b4889754ca7b48eee527168459d304bd3e9c905
pi-rate14 <apoorva.srivastava2019@vitstudent.ac.in>
2022-01-29 15:10:20 +0530

    setting up goroutines

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"gochat/internal/handlers"
 	"log"
 	"net/http"
 )
 
 func main() {
 	mux := routes()
+	log.Println("Starting channel listener")
+	go handlers.ListenToWsChannel()
 	log.Println("Starting web server on port 8080")
 	_ = http.ListenAndServe(":8080", mux)
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,12 +1,16 @@
 package handlers
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/CloudyKit/jet/v6"
 	"github.com/gorilla/websocket"
 )
+
+var wsChan = make(chan WsPayload)
+var clients = make(map[WebSocketConnection]string)
 
 var views = jet.NewSet(
 	jet.NewOSFileSystemLoader("./html"),
@@ -49,9 +53,57 @@ func WsEndpoint(w http.ResponseWriter, r *http.Request){
 	var response WsJsonResponse
 	response.Message = 	`<em><small>Connected to server</small></em>`
 
+	// adding new connections to the map
+	conn := WebSocketConnection{Conn: ws}
+	clients[conn] = ""
+
 	err = ws.WriteJSON(response)
 	if err != nil {
 		log.Println(err)
+	}
+
+	go ListenForWs(&conn)
+}
+
+func ListenForWs(conn *WebSocketConnection) {
+	defer func(){
+		if r := recover(); r != nil{
+			log.Println("Error", fmt.Sprintf("%v", r))
+		}
+	}()
+
+	var payload WsPayload
+
+	for {
+		err := conn.ReadJSON(&payload)
+		if err != nil {
+			// do nothing
+		} else {
+			payload.Conn = *conn
+			wsChan <- payload
+		}
+	}
+}
+
+func ListenToWsChannel() {
+	var response WsJsonResponse
+
+	for {
+		e := <-wsChan
+		response.Action = "Got here"
+		response.Message = fmt.Sprintf("Some message, and action was %s", e.Action)
+		brroadcastToAll(response)
+	}
+}
+
+func brroadcastToAll(response WsJsonResponse) {
+	for client := range clients {
+		err := client.WriteJSON(response)
+		if err != nil {
+			log.Println("websocket err")
+			_ = client.Close()
+			delete(clients,client)
+		}
 	}
 }
 

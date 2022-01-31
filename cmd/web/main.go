bc03e10a53751b446b7383b3e7f7b6209619108b
pi-rate14 <apoorva.srivastava2019@vitstudent.ac.in>
2022-02-01 01:21:09 +0530

    Listed Users

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,8 +8,12 @@ import (
 
 func main() {
 	mux := routes()
+	
 	log.Println("Starting channel listener")
+	
 	go handlers.ListenToWsChannel()
+	
 	log.Println("Starting web server on port 8080")
+	
 	_ = http.ListenAndServe(":8080", mux)
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sort"
 
 	"github.com/CloudyKit/jet/v6"
 	"github.com/gorilla/websocket"
@@ -32,6 +33,7 @@ type WsJsonResponse struct {
 	Action string `json:"action"`
 	Message string `json:"message"`
 	MessageType string `json:"message_type"`
+	ConnectedUsers []string `json:"connected_users"`
 }
 
 type WsPayload struct {
@@ -90,13 +92,42 @@ func ListenToWsChannel() {
 
 	for {
 		e := <-wsChan
-		response.Action = "Got here"
-		response.Message = fmt.Sprintf("Some message, and action was %s", e.Action)
-		brroadcastToAll(response)
+		switch e.Action {
+		case "username":
+			//get a list of all users and send it back via broadcast
+			clients[e.Conn] = e.Username
+			users := getUserList()
+			response.Action = "list_users"
+			response.ConnectedUsers = users
+			broadcastToAll(response)
+		
+		case "left":
+			response.Action = "list_users"
+			delete(clients, e.Conn)
+			users := getUserList()
+			response.ConnectedUsers = users
+			broadcastToAll(response)
+
+		}
+		// response.Action = "Got here"
+		// response.Message = fmt.Sprintf("Some message, and action was %s", e.Action)
+		// broadcastToAll(response)
 	}
 }
 
-func brroadcastToAll(response WsJsonResponse) {
+func getUserList() []string {
+	var userList []string
+	for _, x := range clients {
+		if x != "" {
+			userList = append(userList, x)
+		}
+		
+	}
+	sort.Strings(userList)
+	return userList
+}
+
+func broadcastToAll(response WsJsonResponse) {
 	for client := range clients {
 		err := client.WriteJSON(response)
 		if err != nil {
